6b9def7980096cbbd2c2fec15e61f258dec5999f
Abhinav Shrivastav <152586934+AbhinavShrivastav12@users.noreply.github.com>
2025-09-12 09:32:35 +0545

    done 2

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -5,14 +5,14 @@ import { IoCloudUpload } from 'react-icons/io5'
 const ProfilePicture = () => {
   return (
     <div className='profile-pic'>
-      <p  style={{textAlign:"center"}}>Profile Picture</p>
+      <p  style={{textAlign:"center", paddingBottom:"10px" }}>Profile Picture</p>
       <hr className="h-line"/>
       <picture>
         <img src="/uk.png" alt="Logo" />
       </picture>
       <div className='logo-seen'>
-        <button>Edit Original<IoCloudUpload/></button>
-        <button>Upload New<IoCloudUpload/></button>
+        <button><span>Edit Original</span><IoCloudUpload className='icons'/></button>
+        <button><span>Upload New</span><IoCloudUpload className='icons'/></button>
       </div>
     </div>
   )
